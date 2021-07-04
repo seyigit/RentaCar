d5bfa6e4a0efbe666b59e72e540e04ec6eaa827d
sedat <sedat@DESKTOP-FVUNCE9>
2021-07-04 13:40:12 +0300

    Sql Database

---
+++ CarRental.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [ProductID]
+      ,[ProductName]
+      ,[SupplierID]
+      ,[CategoryID]
+      ,[QuantityPerUnit]
+      ,[UnitPrice]
+      ,[UnitsInStock]
+      ,[UnitsOnOrder]
+      ,[ReorderLevel]
+      ,[Discontinued]
+  FROM [Northwind].[dbo].[Products]
